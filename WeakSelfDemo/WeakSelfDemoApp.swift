9270ea1fc0c31ee8c7444962bf8ec0bfdca1a856
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-03 18:34:06 -0600

    Initial Commit

---
+++ WeakSelfDemo/WeakSelfDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeakSelfDemoApp.swift
+//  WeakSelfDemo
+//
+//  Created by Fred Javalera on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WeakSelfDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
